a7e9db590e2e7d312408bd34a29bf2effe1fbc80
alokhegde2 <hegdealok@yahoo.com>
2021-04-10 10:43:39 +0530

    added orders model

--- routers/orders.js
+++ routers/orders.js
@@ -1,7 +1,57 @@
 const express = require('express')
 const router = express.Router()
+const {Order} = require('../models/order')
 
 
+//GET 
+
+router.get('/',async (req,res) =>{
+    const orderList = await Order.find();
+
+    if(!orderList){
+        res.status(500).json({success:false})
+    }
+    res.send(orderList);
+})
+
+
+//POST
+
+router.post('/',async(req,res) =>{
+    const orderItemsIds = Promise.all(req.body.orderItems.map(async orderitem =>{
+        let newOrderItem = new OrderItem({
+            quantity:orderItem.quantity,
+            product:orderitem.product
+        })
+
+        newOrderItem = await newOrderItem.save();
+
+        return newOrderItem._id
+    }))
+
+    const orderItemsIdsResolved = await orderItemsIds;
+
+    let order = new Order({
+        orderItem:orderItemsIdsResolved,
+        shippingAddress1:req.body.shippingAddress1,
+        shippingAddress2:req.body.shippingAddress2,
+        city:req.body.city,
+        zip:req.body.zip,
+        country:req.body.country,
+        phone:req.body.phone,
+        status:req.body.status,
+        totalPrice:req.body.totalPrice,
+        user:req.body.user,
+    })
+
+    order = await order.save();
+
+    if(!order){
+        return res.status(404).json({'message':'Order cannot be created!'})
+    }
+
+    res.send(order)
+})
 
 
 module.exports =router;
